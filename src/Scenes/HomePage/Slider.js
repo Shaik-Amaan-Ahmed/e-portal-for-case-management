7ee91e3b38fda47cba0b869aba6edc8e54854228
“HarshitGajawada” <gajawadah88@gmail.com>
2023-11-08 15:57:40 +0530

    created image

---
+++ src/Scenes/HomePage/Slider.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Slider() {
+  return (
+    <div>
+      <Box sx={{backgroundImage:"url('https://tshc.gov.in/dds/images/6.jpg')",backgroundRepeat: `no-repeat`,
+            backgroundSize: 'cover',
+            backgroundPosition:"center",
+            borderRadius:"20px",
+            margin:"auto",
+            height: "80vh",
+            width: "98vw",
+            backgroundColor:"rgb(0,0,0,0.5)"
+            
+            }}>
+        </Box>
+    </div>
+  )
+}
