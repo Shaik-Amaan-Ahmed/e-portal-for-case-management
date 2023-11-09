51eaf178fe19c059a39b4d278010138c47457baf
Lohithaksh <lohitl57@gmail.com>
2023-11-09 14:18:19 +0530

    added bootstrap

--- src/Scenes/calendar/calendar.js
+++ src/Scenes/calendar/calendar.js
@@ -3,6 +3,7 @@ import daygridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import { useState } from "react";
 import { v4 as uuid } from "uuid";
+import { useRef } from "react";
 
 const EventItem = ({ info }) => {
   const { event } = info;
@@ -13,41 +14,81 @@ const EventItem = ({ info }) => {
   );
 };
 
-export const MyCalendar = () => {
-  const [events, setEvents] = useState([]);
 
+export const MyCalendar = () => {
+  // const [events, setEvents] = useState([]);
+  const [etitle, setEtitle] = useState([]);
+  const ref = useRef(null);
   const handleSelect = (info) => {
     const { start, end } = info;
-    const eventNamePrompt = prompt("Enter, event name");
-    if (eventNamePrompt) {
-      setEvents([
-        ...events,
-        {
-          start,
-          end,
-          title: eventNamePrompt,
-          id: uuid()
-        }
-      ]);
-    }
-  };
+    ref.current.click();
 
+    // setEvents([
+    //   ...events,
+    //   {
+    //     id: uuid(),
+    //     title: items.title,
+    //     start,
+    //     end
+    //   }
+    // // ]);
+    // console.log(events)
+  }
+  const onChange = (e) => {
+    setEtitle()
+}
   return (
-    <div>
-      <FullCalendar
-        editable
-        selectable
-        events={events}
-        select={handleSelect}
-        headerToolbar={{
-          start: "today prev next",
-          center:"title",
-          end: "dayGridMonth dayGridWeek dayGridDay"
-        }}
-        eventContent={(info) => <EventItem info={info} />}
-        plugins={[daygridPlugin, interactionPlugin]}
-        views={["dayGridMonth", "dayGridWeek", "dayGridDay"]}
-      />
-    </div>
+    <>
+      <button ref={ref} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
+      <div className="modal fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">Add Event</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="title" className="form-label">Enter Title</label>
+                  <input type="text" className="form-control" id="title"  value={etitle} onChange={onChange} name={etitle} aria-describedby="title" />
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="submit" className="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <FullCalendar
+          // editable
+          selectable
+          select={handleSelect}
+          headerToolbar={{
+            start: "today prev next",
+            center: "title",
+            end: "dayGridMonth dayGridWeek dayGridDay"
+          }}
+          events={[
+            { title: 'text', date: '2023-11-08' },
+            { title: 'event 2', date: '2023-11-09' },
+            { title: 'event 2', date: '2023-11-10' },
+            { title: 'event 2', date: '2023-11-11' },
+            { title: 'event 2', date: '2023-11-12' },
+            { title: 'event 2', date: '2023-11-08' },
+            { title: 'event 2', date: '2023-11-08' }
+          ]}
+          eventContent={(info) => <EventItem info={info} />}
+          plugins={[daygridPlugin, interactionPlugin]}
+          views={["dayGridMonth", "dayGridWeek", "dayGridDay"]}
+        />
+      </div>
+    </>
   );
 };
