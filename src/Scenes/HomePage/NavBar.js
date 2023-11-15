f7f9cdea593617de4991ab2f801d69a3a9170601
“HarshitGajawada” <gajawadah88@gmail.com>
2023-11-15 15:48:01 +0530

    pulled navbar changes from susheel

--- src/Scenes/HomePage/NavBar.js
+++ src/Scenes/HomePage/NavBar.js
@@ -18,20 +18,33 @@ const NavBar = () => {
 
   return (
 
-    <div className="p-2 flex justify-between items-center w-screen fixed" style={{backgroundColor: 'rgba(70, 130, 180, 0.8)'}}>
-      <ul>
-        <li className="text-xl inline-flex p-2 mx-5"><a href="/causelist">Cause List</a></li>
-        <li className="text-xl inline-flex p-2 mx-5"><a href="/profiles">Profiles</a></li>
-      </ul>
-      {/* LOGO */}
-      <div className="flex items-center">
-        <a href="/"><img className="rounded-full w-20 h-20" src={Logo} alt="Logo" /></a>
-        <Typography variant="h4" style={{ marginLeft: "10px" }}><span style={{ fontWeight: "bold", fontSize: "1.9em" }}>HIGH COURT</span><br></br>For The State of Telangana</Typography>
+    <div className="p-2 flex justify-between items-center w-screen fixed" style={{ backgroundColor: 'rgba(70, 130, 180, 0.8)' }}>
+      <div className="flex-1">
+        <div className="flex">
+        <ul>
+          <li className="text-xl inline-flex p-2  hover:bg-orange-700 rounded-lg"><a href="/causelist">Cause List</a></li>
+          <li className="text-xl inline-flex p-2 hover:bg-orange-700 rounded-lg"><a href="/profiles">Profiles</a></li>
+          <li className="text-xl inline-flex p-2  hover:bg-orange-700 rounded-lg"><a href="/judgements">Judgements</a></li>
+        </ul>
+        </div>
+       
       </div>
-      <ul className="mr-4">
-        <li className="text-xl inline-flex p-2 mx-5"><a href="/judgments">Judgements</a></li>
-        <li className="text-xl inline-flex p-2 mx-5"><button className="bg-orange-700 py-1 px-5 rounded-lg">Login</button></li>
-      </ul>
+
+      {/* LOGO */}
+      <div className="flex-1 items-center">
+        <div className="flex items-center justify-center">
+          <a href="/"><img className="rounded-full w-20 h-20" src={Logo} alt="Logo" /></a>
+          <Typography variant="h4" style={{ marginLeft: "10px" }}><span style={{ fontWeight: "bold", fontSize: "1.9em" }}><a href="/">HIGH COURT</a></span><br></br>For The State of Telangana</Typography>
+        </div>
+      </div>
+      <div className="flex-1">
+        <div className=" flex justify-end">
+          <ul className="mr-4">
+            <li className="text-xl inline-flex p-2 mx-5 hover:bg-orange-700 py-1 px-5 rounded-lg"><a href="/efiling">E-Filing</a></li>
+            <li className="text-xl inline-flex p-2 mx-5"><a href="/login" className="bg-orange-700 py-1 px-5 rounded-lg">Login</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
 
   );
