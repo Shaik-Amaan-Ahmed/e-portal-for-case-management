2c0c0d5ff13284f51275b83ceb060fcf8ee93a92
“HarshitGajawada” <gajawadah88@gmail.com>
2023-12-14 14:39:51 +0530

    updated notices

--- src/Scenes/HomePage/NoticeBoard.js
+++ src/Scenes/HomePage/NoticeBoard.js
@@ -11,16 +11,16 @@ export default function
        
        <ul>
          <li>
-           <a href="http://" target="_blank" rel="noopener noreferrer">"NJA, BHOPAL - Relief Arrangements - Nomination of (02) Judicial Officers of the District Judiciary to participate in the Academic Programme (No.P-1372) scheduled on 18.11.2023 and 19.11.2023 at NJA, Bhopal - Orders - Issued - B.Spl., dated 07-11-2023"</a>
+           <a href="https://tshc.gov.in/documents/bspl_2_2023_12_13_17_43_41.pdf" target="_blank" rel="noopener noreferrer">"JUNIOR CIVIL JUDGES - Transfers and Postings of Junior Civil Judges - Orders - Issued - B.Special Section, dated 13-12-2023"</a>
          </li>
          <li>
-           <a href="http://" target="_blank" rel="noopener noreferrer">"NJA, BHOPAL - Relief Arrangements - Nomination of (02) Judicial Officers of the District Judiciary to participate in the Academic Programme (No.P-1372) scheduled on 18.11.2023 and 19.11.2023 at NJA, Bhopal - Orders - Issued - B.Spl., dated 07-11-2023"</a>
+           <a href="https://tshc.gov.in/documents/jspladm_2_2023_12_12_15_21_19.pdf" target="_blank" rel="noopener noreferrer">"Full Court Reference in the First Court Hall on 14-12-2023 at 3:45 pm to condole the demise of Honourable Sri Justice M.H.S.Ansari, former Judge of erstwhile High Court of A. P. and former Judge of Calcutta High Court - J.Spl.(Admn.), dt. 12-12-2023"</a>
          </li>
          <li>
-           <a href="http://" target="_blank" rel="noopener noreferrer">"NJA, BHOPAL - Relief Arrangements - Nomination of (02) Judicial Officers of the District Judiciary to participate in the Academic Programme (No.P-1372) scheduled on 18.11.2023 and 19.11.2023 at NJA, Bhopal - Orders - Issued - B.Spl., dated 07-11-2023"</a>
+           <a href="https://tshc.gov.in/documents/splofficer_2_2023_12_08_15_49_33.pdf" target="_blank" rel="noopener noreferrer">"TSHC-Tender Notification,dt. 08.12.2023 relating to printing and supply of calendars of the High Court for the State of Telangana and District Judiciary in the State of Telangana for the year 2024-Reg. Spl. Officer Section."</a>
          </li>
          <li>
-           <a href="http://" target="_blank" rel="noopener noreferrer">"NJA, BHOPAL - Relief Arrangements - Nomination of (02) Judicial Officers of the District Judiciary to participate in the Academic Programme (No.P-1372) scheduled on 18.11.2023 and 19.11.2023 at NJA, Bhopal - Orders - Issued - B.Spl., dated 07-11-2023"</a>
+           <a href="https://tshc.gov.in/documents/reccell_2_2023_11_29_18_42_51.pdf" target="_blank" rel="noopener noreferrer">"Notification regarding the Re-Conducting of the Skill Test to the candidates who applied to the posts of Typists and Copyists vide Notification Nos. 8/2023 and 09/2023 dated 12-05-2023 under TJMS Rules."</a>
          </li>
        </ul>
       </Box>
