b436b85bbf89fc72e3973723842d9c47d66c41ba
“HarshitGajawada” <gajawadah88@gmail.com>
2023-12-19 14:45:19 +0530

    integrated homepage to website

--- client/src/Scenes/HomePage/NavBar.js
+++ client/src/Scenes/HomePage/NavBar.js
@@ -9,36 +9,72 @@ const NavBar = () => {
   const colorMode = useContext(ColorModeContext);
 
   return (
+    <div
+      style={{
+        padding: '8px',
+        backgroundColor: "rgba(70, 130, 180, 1)",
+        display: "flex",
+        justifyContent: "space-between",
+        position: "fixed",
+        width: "100%",
+        alignItems: "center",
+        height: "max-content",
 
-    <div className="p-2 flex justify-between items-center w-screen fixed" style={{ backgroundColor: 'rgba(70, 130, 180, 1)' }}>
+      }}
+    >
       <div className="flex-1">
         <div className="flex">
-        <ul>
-          <li className="text-xl inline-flex p-2  hover:bg-orange-700 rounded-lg"><a href="/causelist">Cause List</a></li>
-          <li className="text-xl inline-flex p-2 hover:bg-orange-700 rounded-lg"><a href="/profiles">Profiles</a></li>
-          <li className="text-xl inline-flex p-2  hover:bg-orange-700 rounded-lg"><a href="/judgements">Judgements</a></li>
-        </ul>
+          <ul>
+            <li className="text-xl inline-flex p-2  hover:bg-orange-700 rounded-lg">
+              <a href="/causelist">Cause List</a>
+            </li>
+            <li className="text-xl inline-flex p-2 hover:bg-orange-700 rounded-lg">
+              <a href="/profiles">Profiles</a>
+            </li>
+            <li className="text-xl inline-flex p-2  hover:bg-orange-700 rounded-lg">
+              <a href="/judgements">Judgements</a>
+            </li>
+          </ul>
         </div>
-       
       </div>
 
       {/* LOGO */}
       <div className="flex-1 items-center">
         <div className="flex items-center justify-center">
-          <a href="/"><img className="rounded-full w-20 h-20" src={Logo} alt="Logo" /></a>
-          <Typography variant="h4" style={{ marginLeft: "10px" }}><span style={{ fontWeight: "bold", fontSize: "1.9em" }}><a href="/">HIGH COURT</a></span><br></br>For The State of Telangana</Typography>
+          <a href="/">
+            <img className="rounded-full w-20 h-20" src={Logo} alt="Logo" />
+          </a>
+          <Typography variant="h3" style={{ marginLeft: "10px" }}>
+            <span style={{ fontWeight: "bold", justifyContent:"center", alignItems:"center", display:"flex" }}>
+              <a style={{ display: "flex", justifyContent: "center" , width:"100%"}} href="/">
+                HIGH COURT OF
+              </a>
+            </span>
+            <span style={{ fontWeight: "bold" }}>
+              <a style={{ display: "flex", justifyContent: "center" }} href="/">
+                TELANGANA
+              </a>
+            </span>
+          </Typography>
         </div>
       </div>
       <div className="flex-1">
         <div className=" flex justify-end">
           <ul className="mr-4">
-            <li className="text-xl inline-flex p-2 mx-5 hover:bg-orange-700 rounded-lg"><a href="/efiling">E-Filing</a></li>
-            <li className="text-xl inline-flex p-2 mx-5 bg-orange-700 rounded-lg"><a href="/login" className="">Login</a></li>
+            <li className="text-xl inline-flex p-2 mx-5 hover:bg-orange-700 rounded-lg">
+              <a href="/register">
+                Register
+              </a>
+            </li>
+            <li className="text-xl inline-flex p-2 mx-5 bg-orange-700 rounded-lg">
+              <a href="/login" className="">
+                Login
+              </a>
+            </li>
           </ul>
         </div>
       </div>
     </div>
-
   );
 };
 
