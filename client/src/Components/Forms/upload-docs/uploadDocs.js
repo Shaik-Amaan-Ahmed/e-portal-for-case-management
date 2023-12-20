ccff37ed5f70d152cc02a990dfb6220b58646aa6
Shaik Amaan <117620825+boss07-pro@users.noreply.github.com>
2023-12-20 16:21:43 +0530

    Added e-docs changes

--- client/src/Components/Forms/upload-docs/uploadDocs.js
+++ client/src/Components/Forms/upload-docs/uploadDocs.js
@@ -3,19 +3,40 @@ import "./uploadDocs.css";
 import { Typography } from '@mui/material';
 
 const UploadDocs = () => {
-    const [selectedFile, setSelectedFile] = useState(null);
-    const [title ,setTitle] = useState('');
-    const [fileName, setFileName] = useState('');
+    const [selectedPetition, setSelectedPetition] = useState(null);
+    const [selectedAadhar, setSelectedAadhar] = useState(null);
+    const [petition ,setPetition] = useState('');
+    const [aadhar ,setAadhar] = useState('');
+    const [petitionName, setPetitionName] = useState('');
+    const [aadharName, setAadharName] = useState('');
 
-    const fileSelectedHandler = event => {
-        setSelectedFile(event.target.files[0]);
-        setFileName(event.target.files[0].name);
+    const petitionSelectedHandler = event => {
+        const file1=event.target.files[0];
+        setSelectedPetition(file1);
+        setPetitionName(file1.name);
     }
 
-    const fileUploadHandler = () => {
-        const formData = new FormData();
-        formData.append({title}, selectedFile); // append the file to the form data
-        // Now you can send this form data to your server
+    const aadharSelectedHandler = event => {
+        const file2=event.target.files[0];
+        setSelectedAadhar(file2);
+        setAadharName(file2.name);
+    }
+    const petitionChangeHandler = event => {
+        setPetition(event.target.value);
+    }
+    
+    const aadharChangeHandler = event => {
+        setAadhar(event.target.value);
+    }
+
+    const petitionUploadHandler = () => {
+        const petitionData = new petitionData();
+        petitionData.append(petition, selectedPetition);
+    }
+
+    const aadharUploadHandler = () => {
+        const aadharData = new aadharData();
+        aadharData.append(aadhar, selectedAadhar);
     }
 
     return (
@@ -23,38 +44,28 @@ const UploadDocs = () => {
             <Typography variant='h4'>Upload Docs</Typography>
             <div className='upload-div'>
                 <div className="doc-title">
-                <Typography style={{display:"flex",marginRight:"20px"}} variant='h5'>Petetion</Typography>
+                    <Typography style={{display:"flex",marginRight:"20px"}} variant='h5'>Petition</Typography>
                 </div>
-               <div className='doc-input'>
-               <input type="text" className='docs-title' placeholder='Title'/>
-                <input type="file" id="file" onChange={fileSelectedHandler} placeholder='Select Files' style={{display: 'none'}} />
-                <label htmlFor="file" className="custom-file-upload">Browse</label>
-                <span style={{
-                    // display: 'flex',
-                    alignItems: 'center',
-                    border: "0.1px solid grey",
-                    padding:"10px"
-                }}>{fileName}</span>
-               </div>
-                
+                <div className='doc-input'>
+                    <input type="text" className='docs-title' placeholder='Title' onChange={petitionChangeHandler} />
+                    <input type="file" id="petition-file" onChange={petitionSelectedHandler} placeholder='Select Files' style={{display: 'none'}} />
+                    <label htmlFor="petition-file" className="custom-file-upload">Browse</label>
+                    <span style={{display:"flex"}}>{petitionName}</span>
+                </div>
             </div>
+            
             <div className='upload-div'>
                 <div className="doc-title">
-                <Typography style={{display:"flex",marginRight:"20px"}} variant='h5'>Aadhar Details</Typography>
+                    <Typography style={{display:"flex",marginRight:"20px"}} variant='h5'>Aadhar</Typography>
                 </div>
-               <div className='doc-input'>
-               <input type="text" className='docs-title' placeholder='Title'/>
-                <input type="file" id="file" onChange={fileSelectedHandler} placeholder='Select Files' style={{display: 'none'}} />
-                <label htmlFor="file" className="custom-file-upload">Browse</label>
-                <span style={{
-                    // display: 'flex',
-                    alignItems: 'center',
-                    border: "0.1px solid grey",
-                    padding:"10px"
-                }}>{fileName}</span>
-               </div>
-                
+                <div className='doc-input'>
+                    <input type="text" className='docs-title' placeholder='Title' onChange={aadharChangeHandler} />
+                    <input type="file" id="aadhar-file" onChange={aadharSelectedHandler} placeholder='Select Files' style={{display: 'none'}} />
+                    <label htmlFor="aadhar-file" className="custom-file-upload">Browse</label>
+                    <span style={{display:"flex"}}>{aadharName}</span>
+                </div>
             </div>
+            
         </div>
     )
 }
