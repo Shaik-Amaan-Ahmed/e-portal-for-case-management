c469dcb120114a26e5022c81eaaba432cd996887
“HarshitGajawada” <gajawadah88@gmail.com>
2023-12-23 15:53:13 +0530

    added navbar to profiles tab

--- client/src/Scenes/HomePage/NavBar.js
+++ client/src/Scenes/HomePage/NavBar.js
@@ -19,7 +19,7 @@ const NavBar = () => {
         width: "100%",
         alignItems: "center",
         height: "max-content",
-
+        zIndex:1000
       }}
     >
       <div className="flex-1">
