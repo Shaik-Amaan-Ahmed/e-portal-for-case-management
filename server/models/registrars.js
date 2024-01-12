2cd32cde48e7f79abc3c93507d99ed9b553d4cc1
“HarshitGajawada” <gajawadah88@gmail.com>
2024-01-12 21:51:30 +0530

    Update registrars.js

--- server/models/registrars.js
+++ server/models/registrars.js
@@ -2,12 +2,11 @@ const mongoose = require("mongoose");
 const Schema  = mongoose.Schema;
 
 const Registrar = mongoose.model("registrars", new Schema({ 
-    username: String,
-    password: String,
+    name: {type: String, required: true},
+    email: {type: String, required: true},
     token: {type: String},
     password: {type:String},
-    phone: {type: String, required: true}
-    
+    phone: {type: String, required: true},
 }));
 
 module.exports = Registrar;
